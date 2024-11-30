34cfc6055ec19c60b974d8343111a61725deac9b
Nataliia Ruda <nataliiar754@gmail.com>
2024-11-30 23:11:14 +0100

    Added map in main page

--- js/index.js
+++ js/index.js
@@ -15,6 +15,19 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(showNextImage, 4000);
 }); 
 
+// map starts here 
+var map = L.map('map').setView([51.5719, 4.7683], 7);
+// Style URL format in XYZ PNG format; see our documentation for more options
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+}).addTo(map)
+
+var marker = L.marker([51.2085, 3.2251]).addTo(map); //Brugge
+var marker = L.marker([51.5072, 0.1276]).addTo(map); //London 
+var marker = L.marker([50.8229, 0.1363]).addTo(map); //Brighton 
+var marker = L.marker([51.9244, 4.4777]).addTo(map); //Rotterdam 
+
 
 
 
